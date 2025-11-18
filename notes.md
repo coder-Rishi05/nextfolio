a2d57617e2114c7669b2ef497ef44e430441dba1
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-11-19 00:30:22 +0530

    updated notes

--- notes.md
+++ notes.md
@@ -4,9 +4,4 @@ next learning : https://nextjs.org/learn/dashboard-app
 
 next-learn repo (for mockdata or json) : https://github.com/vercel/next-learn/tree/main/dashboard/starter-example
 
-# Nextfolio
-
-Desc:
-My very first design first nextjs kind of portfolio site.
-
-
+docs : https://nextjs.org/docs
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div>
+      <h1 className="fontSt text-7xl tracking-tight px-90 text-center font-extrabold  ">
+        Welcome
+      </h1>
+      <p className=" w-1/2 px-40 ">
+        Hi, I'm Yaron ( Ya • rone ) — a Product Designer based in beautiful
+        Connecticut, where I live with my amazing wife, two wonderful daughters,
+        a goofy pup, and a grumpy old cat. I’ve worn many hats in the past 25
+        years — product designer, entrepreneur, mentor, and strategist. I’ve
+        worked for world-class companies, built startups from scratch, led them
+        to profitability or acquisition, and even experienced the thrill of an
+        IPO as an early-stage employee. Outside of work, you’ll find me reading
+        historical non-fiction, making music, cooking up a storm, or traveling
+        to new places. Welcome to my website, feel free to poke around.
+      </p>
+    </div>
+  );
+};
+
+export default Profile;
