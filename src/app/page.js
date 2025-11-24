b34063b0a9a38156140014d08ce1493e0f560c97
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-11-24 10:24:16 +0530

    responsive nav

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,9 @@ import Profile from "../components/Profile";
 import Nav from "../components/Nav.jsx";
 export default function Home() {
   return (
-    <div className=" w-full ">
+    <div className=" w-full bg-fuchsia-500 ">
       <Nav />
-      <Profile />
+      {/* <Profile /> */}
     </div>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,20 +4,23 @@ import { FiMoon } from "react-icons/fi";
 
 const Nav = () => {
   return (
-    <header className="py-2  ">
-      <nav className="max-w-6xl mx-auto w-full flex items-center justify-between px-42">
+    <header className="w-full py-3 px-5 bg-yellow-200">
+      <nav className="w-full flex items-center justify-between lg:justify-around">
         {/* Logo */}
-        <div className="flex  items-center">
-          <img
-            className="w-80 object-contain"
+        <div className="w-24 sm:w-32 lg:w-40">
+          <Image
             src="/logo-removebg-preview.png"
             alt="logo"
+            width={200}
+            height={80}
+            className="object-contain"
+            priority
           />
         </div>
 
         {/* Moon Icon */}
-        <button className="flex cursor-pointer items-center">
-          <FiMoon size={25} className="text-white" />
+        <button className="p-2 rounded-md cursor-pointer">
+          <FiMoon size={25} className="text-gray-900" />
         </button>
       </nav>
     </header>
