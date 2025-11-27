c9626020a28a9f81cdfa5953fa9adfd404fa9d4c
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-11-27 23:03:22 +0530

    completed responsive of nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,23 +4,23 @@ import { FiMoon } from "react-icons/fi";
 
 const Nav = () => {
   return (
-    <header className="w-full py-3 px-10">
-      <nav className="w-full flex items-center justify-between lg:justify-around">
+    <header className="w-full py-2 px-4 sm:py-3 sm:px-6 md:px-10">
+      <nav className="w-full flex items-center justify-between">
         {/* Logo */}
-        <div className="w-24 sm:w-32 lg:w-40">
+        <div className="w-20 sm:w-24 md:w-32 lg:w-40">
           <Image
             src="/logo-removebg-preview.png"
             alt="logo"
             width={200}
             height={80}
-            className="object-contain"
+            className="object-contain w-full h-auto"
             priority
           />
         </div>
 
         {/* Moon Icon */}
-        <button className="p-2 rounded-md cursor-pointer">
-          <FiMoon size={25} className="text-gray-900" />
+        <button className="p-1.5 sm:p-2 rounded-md cursor-pointer hover:bg-gray-100 transition-colors">
+          <FiMoon size={20} className="sm:w-6 sm:h-6 text-gray-900" />
         </button>
       </nav>
     </header>
