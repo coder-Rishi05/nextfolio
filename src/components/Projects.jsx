1f58abb0f86f0e74facecc366505df268fee3c41
coder-Rishi05 <rawatrishi1121@gmail.com>
2026-01-03 21:37:05 +0530

    completed project page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,17 +1,235 @@
 import React from "react";
+import { FaLink } from "react-icons/fa";
 
 const Projects = () => {
   return (
-    <section className="w-full min-h-screen bg-red-400 border-t border-amber-200 flex flex-col items-center py-12">
+    <section className="w-full min-h-screen border-t border-white flex flex-col items-center py-12">
       {/* Section heading */}
-      <div className="w-full max-w-5xl mx-auto px-4 sm:px-6 md:px-8 mb-10">
-        <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl tracking-tight font-extrabold">
+      <div className="w-full  max-w-5xl mx-auto px-4 sm:px-6 md:px-8 mb-10">
+        {/* <div className="w-1/2 bg-amber-300 px-4 sm:px-6 md:px-8 mb-10"> */}
+        <h1 className="text-4xl px-40  sm:text-5xl md:text-6xl lg:text-7xl tracking-tight font-extrabold">
           Projects
         </h1>
       </div>
 
+      <div className="project w-[75%] px-24 ">
+        <div className="top flex   justify-between px-40 ">
+          <div className="left">
+            <h1>ServiceNow</h1>
+            <p>UX Architect (AI Platform team)</p>
+            <small>2023 - Present</small>
+          </div>
+          <div className="right">
+            <div className="link">
+              <FaLink />
+            </div>
+          </div>
+        </div>
+        <div className="bottom px-24 py-10 ">
+          <div className="content px-18">
+            Designing and evangelizing unified, scalable, end-to-end,
+            platform-centric systems and product strategies. UX Architects have
+            a birds-eye view and are concerned with the overall structure and
+            flow of multiple tools, features, and capabilities within an
+            ecosystem.
+            <div className="highlight py-6">
+              <ul className="">
+                <li className="list-disc py-2">
+                  Developed prompt-to-experience tool by establishing design
+                  system ontologies and integrating them into MCP servers for
+                  LLM consumption.
+                </li>
+                <li className="list-disc py-2">
+                  {" "}
+                  Established initial AI UX patterns for the platform.{" "}
+                </li>
+                <li>
+                  Maintained and modified platform wide navigational structures.{" "}
+                </li>
+                <li className="list-disc py-2">
+                  Supported consolidation of 50+ developer tools into one
+                  unified ecosystem named ServiceNow Studio.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="project w-[75%] px-24 ">
+        <div className="top flex   justify-between px-40 ">
+          <div className="left">
+            <h1>ServiceNow</h1>
+            <p>UX Architect (AI Platform team)</p>
+            <small>2023 - Present</small>
+          </div>
+          <div className="right">
+            <div className="link">
+              <FaLink />
+            </div>
+          </div>
+        </div>
+        <div className="bottom px-24 py-10 ">
+          <div className="content px-18">
+            Designing and evangelizing unified, scalable, end-to-end,
+            platform-centric systems and product strategies. UX Architects have
+            a birds-eye view and are concerned with the overall structure and
+            flow of multiple tools, features, and capabilities within an
+            ecosystem.
+            <div className="highlight py-6">
+              <ul className="">
+                <li className="list-disc py-2">
+                  Developed prompt-to-experience tool by establishing design
+                  system ontologies and integrating them into MCP servers for
+                  LLM consumption.
+                </li>
+                <li className="list-disc py-2">
+                  {" "}
+                  Established initial AI UX patterns for the platform.{" "}
+                </li>
+                <li>
+                  Maintained and modified platform wide navigational structures.{" "}
+                </li>
+                <li className="list-disc py-2">
+                  Supported consolidation of 50+ developer tools into one
+                  unified ecosystem named ServiceNow Studio.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="project w-[75%] px-24 ">
+        <div className="top flex   justify-between px-40 ">
+          <div className="left">
+            <h1>ServiceNow</h1>
+            <p>UX Architect (AI Platform team)</p>
+            <small>2023 - Present</small>
+          </div>
+          <div className="right">
+            <div className="link">
+              <FaLink />
+            </div>
+          </div>
+        </div>
+        <div className="bottom px-24 py-10 ">
+          <div className="content px-18">
+            Designing and evangelizing unified, scalable, end-to-end,
+            platform-centric systems and product strategies. UX Architects have
+            a birds-eye view and are concerned with the overall structure and
+            flow of multiple tools, features, and capabilities within an
+            ecosystem.
+            <div className="highlight py-6">
+              <ul className="">
+                <li className="list-disc py-2">
+                  Developed prompt-to-experience tool by establishing design
+                  system ontologies and integrating them into MCP servers for
+                  LLM consumption.
+                </li>
+                <li className="list-disc py-2">
+                  {" "}
+                  Established initial AI UX patterns for the platform.{" "}
+                </li>
+                <li>
+                  Maintained and modified platform wide navigational structures.{" "}
+                </li>
+                <li className="list-disc py-2">
+                  Supported consolidation of 50+ developer tools into one
+                  unified ecosystem named ServiceNow Studio.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="project w-[75%] px-24 ">
+        <div className="top flex   justify-between px-40 ">
+          <div className="left">
+            <h1>ServiceNow</h1>
+            <p>UX Architect (AI Platform team)</p>
+            <small>2023 - Present</small>
+          </div>
+          <div className="right">
+            <div className="link">
+              <FaLink />
+            </div>
+          </div>
+        </div>
+        <div className="bottom px-24 py-10 ">
+          <div className="content px-18">
+            Designing and evangelizing unified, scalable, end-to-end,
+            platform-centric systems and product strategies. UX Architects have
+            a birds-eye view and are concerned with the overall structure and
+            flow of multiple tools, features, and capabilities within an
+            ecosystem.
+            <div className="highlight py-6">
+              <ul className="">
+                <li className="list-disc py-2">
+                  Developed prompt-to-experience tool by establishing design
+                  system ontologies and integrating them into MCP servers for
+                  LLM consumption.
+                </li>
+                <li className="list-disc py-2">
+                  {" "}
+                  Established initial AI UX patterns for the platform.{" "}
+                </li>
+                <li>
+                  Maintained and modified platform wide navigational structures.{" "}
+                </li>
+                <li className="list-disc py-2">
+                  Supported consolidation of 50+ developer tools into one
+                  unified ecosystem named ServiceNow Studio.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="project w-[75%] px-24 ">
+        <div className="top flex   justify-between px-40 ">
+          <div className="left">
+            <h1>ServiceNow</h1>
+            <p>UX Architect (AI Platform team)</p>
+            <small>2023 - Present</small>
+          </div>
+          <div className="right">
+            <div className="link">
+              <FaLink />
+            </div>
+          </div>
+        </div>
+        <div className="bottom px-24 py-10 ">
+          <div className="content px-18">
+            Designing and evangelizing unified, scalable, end-to-end,
+            platform-centric systems and product strategies. UX Architects have
+            a birds-eye view and are concerned with the overall structure and
+            flow of multiple tools, features, and capabilities within an
+            ecosystem.
+            <div className="highlight py-6">
+              <ul className="">
+                <li className="list-disc py-2">
+                  Developed prompt-to-experience tool by establishing design
+                  system ontologies and integrating them into MCP servers for
+                  LLM consumption.
+                </li>
+                <li className="list-disc py-2">
+                  {" "}
+                  Established initial AI UX patterns for the platform.{" "}
+                </li>
+                <li>
+                  Maintained and modified platform wide navigational structures.{" "}
+                </li>
+                <li className="list-disc py-2">
+                  Supported consolidation of 50+ developer tools into one
+                  unified ecosystem named ServiceNow Studio.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
       {/* Content */}
-      <div className="w-full max-w-4xl mx-auto px-4 sm:px-6 md:px-8 flex flex-col gap-4">
+      {/* <div className="w-full max-w-4xl mx-auto px-4 sm:px-6 md:px-8 flex flex-col gap-4">
         <p className="text-sm sm:text-base md:text-lg font-mono leading-relaxed">
           Hi, I'm ऋषिः ( ऋषभः • २. ) — a web designer and developer. I create
           websites not just to do the work but to make it an unforgettable
@@ -28,7 +246,7 @@ const Projects = () => {
           Outside of work, you'll find me reading books about power, influence,
           psychology, and fiction.
         </p>
-      </div>
+      </div> */}
     </section>
   );
 };
