647a3c7e407919373efffc45234d99b8c6eadfbc
coder-Rishi05 <rawatrishi1121@gmail.com>
2026-01-14 15:37:17 +0530

    added motion

--- src/components/navs/Navbar.jsx
+++ src/components/navs/Navbar.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import Link from "next/link";
+import { motion } from "motion/react";
+
 const Navbar = () => {
   return (
     <div className="flex items-center justify-center border border-amber-50 gap-16">
-      <li className="list-none border-r-2  border-amber-50 px-4  py-4  ">
+      <motion.li
+        whileHover={{ x: 100 }}
+        className="list-none border-r-2  border-amber-50 px-4  py-4  "
+      >
         <a className="font-normal uppercase text-lg" href="">
           Skills
         </a>
-      </li>
+      </motion.li>
       <li className="list-none border-r-2 border-amber-50 px-4  py-4  ">
-        <a className="font-normal uppercase text-lg" href="">
+        <a
+          whileHover={{ scale: 1.1 }}
+          className="font-normal uppercase text-lg"
+          href=""
+        >
           Projects
         </a>
       </li>
